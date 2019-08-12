9a7ef2e9e100c458f63dd4fa94fa817ca8be6a76
Yen Mai <yenmai478@gmail.com>
2019-08-12 15:24:38 +0700

    Change git url

--- config.js
+++ config.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  url: "https://yen-blog.netlify.com/",
-  pathPrefix: "/",
-  title: "Blog by Yen Mai",
+  url: 'https://yen-blog.netlify.com/',
+  pathPrefix: '/',
+  title: 'Blog by Yen Mai',
   subtitle:
-    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
-  copyright: "© All rights reserved.",
-  disqusShortname: "",
+    'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: "UA-73379983-2",
+  googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
-      label: "Articles",
-      path: "/",
+      label: 'Articles',
+      path: '/',
     },
     {
-      label: "About me",
-      path: "/pages/about",
+      label: 'About me',
+      path: '/pages/about',
     },
     {
-      label: "Contact me",
-      path: "/pages/contacts",
+      label: 'Contact me',
+      path: '/pages/contacts',
     },
   ],
   author: {
-    name: "Yen Mai",
-    photo: "/photo.jpg",
+    name: 'Yen Mai',
+    photo: '/photo.jpg',
     bio:
-      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+      'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: "yenmai478@gmail.com",
-      telegram: "#",
-      twitter: "#",
-      github: "https://github.com/no197",
-      rss: "#",
-      vkontakte: "#",
+      email: 'yenmai478@gmail.com',
+      telegram: '#',
+      twitter: '#',
+      github: '/no197',
+      rss: '#',
+      vkontakte: '#',
     },
   },
 };
